0dee241717900881d88e59bf9aa60947d651ffad
karan <karanb479@gmail.com>
2024-08-25 17:30:45 +0530

    ExpenceItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Expence from './Components/ExpenceItems';
 
 function App() {
   return (
     <div>
       <h1>React</h1>
       <p>I am learnig react</p>
+      <Expence />
     </div>
   )
 }
---
+++ src/Components/ExpenceItems.jsx
@@ -0,0 +1,12 @@
+function ExpenceItem(){
+    return (
+        <>
+            <p>This is Expence Items!</p>
+            <p>Food - $5</p>
+            <p>Petrol - $20</p>
+            <p>Movie - $10</p>
+        </>
+     )
+}
+
+export default ExpenceItem
