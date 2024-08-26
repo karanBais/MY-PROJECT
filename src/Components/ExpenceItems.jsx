ded9d1f08cf00261fb1bb81c64b7fa6de58e5e19
karan <karanb479@gmail.com>
2024-08-26 14:43:43 +0530

    ExpenseCss

--- src/Components/ExpenceItems.jsx
+++ src/Components/ExpenceItems.jsx
@@ -1,12 +1,26 @@
-function ExpenceItem(){
-    return (
-        <>
-            <p>This is Expence Items!</p>
-            <p>Food - $5</p>
-            <p>Petrol - $20</p>
-            <p>Movie - $10</p>
-        </>
-     )
+// Write yoour code at relevant places in the code below:
+
+import "./ExpenseItems.css";
+
+function ExpenseItems() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore"
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">
+      {expenseLocation}
+      </div>
+
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenceItem
+export default ExpenseItems;
